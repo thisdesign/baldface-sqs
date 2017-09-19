56003caadb573f65005eaf1aebd5601df31437c8
kitajchuk <kitajchuk@gmail.com>
2017-09-19 09:39:29 -0700

    Remove cover controllers...

--- source/js/class/Controllers.js
+++ source/js/class/Controllers.js
@@ -1,7 +1,6 @@
 import * as core from "../core";
 import ImageController from "./ImageController";
 import AnimateController from "./AnimateController";
-import CoverController from "./CoverController";
 import QueryController from "./QueryController";
 import CarouselController from "./CarouselController";
 import FormController from "./FormController";
@@ -61,7 +60,6 @@ class Controllers {
         this.controllers = [];
 
         this.push( "animate", this.element.find( core.config.animSelector ), AnimateController, true );
-        this.push( "cover", this.element.find( core.config.coverSelector ), CoverController, true );
         this.push( "carousel", this.element.find( core.config.carouselSelector ), CarouselController, true );
         this.push( "form", core.dom.body.find( core.config.formSelector ), FormController, true );
         this.push( "feed", this.element.find( core.config.feedSelector ), FeedController, true );
