ebfe4a4d89886debe7580a01b8e27fb126f88443
kitajchuk <kitajchuk@gmail.com>
2017-09-28 14:44:42 -0700

    Embed video handling updates

--- source/js/views/embed-video.js
+++ source/js/views/embed-video.js
@@ -3,12 +3,18 @@ import paramalama from "paramalama";
 
 
 export default ( data ) => {
-    const url = (data.provider === "youtube" ? `https://www.youtube.com/embed/${paramalama( data.url ).v}?autoplay=1&loop=1&controls=0&modestbranding=1` : `https://player.vimeo.com/video/${data.url.split( "/" ).pop()}?autoplay=1&loop=1&background=1`);
+    const params = paramalama( data.url );
+    const url = data.url.replace( /\?.*?$/, "" );
+    const qrs = (data.provider === "vimeo" ? "?api=1" : "?controls=0&modestbranding=1&enablejsapi=1");
+    const path = (data.provider === "vimeo" ? "https://player.vimeo.com/video/" : "https://www.youtube.com/embed/");
+    const id = (data.provider === "youtube" && params.v ? params.v : url.split( "/" ).pop());
+    const source = `${path}${id}${qrs}`;
+    const aspect = data.height / data.width * 100;
 
     return `
         <div class="embed">
-            <div class="embed__aspect" style="padding-bottom:${data.height / data.width * 100}%;">
-                <iframe class="embed__element" src="${url}" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+            <div class="embed__aspect" style="padding-bottom:${aspect}%;">
+                <iframe class="embed__element" src="${source}" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
             </div>
         </div>
     `;
--- source/js/views/overlay-video.js
+++ source/js/views/overlay-video.js
@@ -5,7 +5,7 @@ import paramalama from "paramalama";
 export default ( item ) => {
     const params = paramalama( item.video.url );
     const url = item.video.url.replace( /\?.*?$/, "" );
-    const qrs = (item.video.provider === "vimeo" ? "?autoplay=1" : "?autoplay=1&modestbranding=1");
+    const qrs = (item.video.provider === "vimeo" ? "?autoplay=1&api=1" : "?autoplay=1&modestbranding=1&enablejsapi=1");
     const path = (item.video.provider === "vimeo" ? "https://player.vimeo.com/video/" : "https://www.youtube.com/embed/");
     const id = (item.video.provider === "youtube" && params.v ? params.v : url.split( "/" ).pop());
     const source = `${path}${id}${qrs}`;
