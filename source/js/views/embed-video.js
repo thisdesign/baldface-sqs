4045aad60bbc94598f6a96432230e5992d6f55f0
kitajchuk <kitajchuk@gmail.com>
2017-10-03 10:20:45 -0700

    add background parameter for vimeo embeds

--- source/js/views/embed-video.js
+++ source/js/views/embed-video.js
@@ -5,7 +5,7 @@ import paramalama from "paramalama";
 export default ( data ) => {
     const params = paramalama( data.url );
     const url = data.url.replace( /\?.*?$/, "" );
-    const qrs = (data.provider === "vimeo" ? "?&autoplay=1&api=1&loop=0" : "?&autoplay=1&loop=0&controls=0&modestbranding=1&enablejsapi=1");
+    const qrs = (data.provider === "vimeo" ? "?&background=1&autoplay=1&api=1&loop=0" : "?&autoplay=1&loop=0&controls=0&modestbranding=1&enablejsapi=1");
     const path = (data.provider === "vimeo" ? "https://player.vimeo.com/video/" : "https://www.youtube.com/embed/");
     const id = (data.provider === "youtube" && params.v ? params.v : url.split( "/" ).pop());
     const source = `${path}${id}${qrs}`;
