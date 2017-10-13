ba41d1e36392878e9dfa1583c63ce2a21a109cfe
kitajchuk <kitajchuk@gmail.com>
2017-10-12 20:39:44 -0700

    add typography classes to feed controls

--- source/js/views/feed-filter.js
+++ source/js/views/feed-filter.js
@@ -1,9 +1,9 @@
 export default ( cats ) => {
     return `
-        <a class="feed__category js-feed-filter-cat a -column is-active" href="#All">All</a>
-        <a class="feed__category js-feed-filter-cat a -column" href="#Instagram">Instagram</a>
+        <a class="feed__category js-feed-filter-cat p a -column is-active" href="#All">All</a>
+        <a class="feed__category js-feed-filter-cat p a -column" href="#Instagram">Instagram</a>
         ${cats.map(( cat ) => {
-            return `<a class="feed__category js-feed-filter-cat a -column" href="#${cat}">${cat}</a>`;
+            return `<a class="feed__category js-feed-filter-cat p a -column" href="#${cat}">${cat}</a>`;
 
         }).join( "" )}
     `;
