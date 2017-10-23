d72fcab0fefa5cf4e32c29a60979263d36e9ff61
kitajchuk <kitajchuk@gmail.com>
2017-10-23 10:58:02 -0700

    hover state for instagram widget

--- source/js/views/instagram-items.js
+++ source/js/views/instagram-items.js
@@ -1,7 +1,16 @@
 export default ( json ) => {
     return json.data.map(( item ) => {
+        const rTags = /(<([^>]+)>)/ig;
+        const caption = `<div class="grid__title p p--h3">${item.caption.text.replace( rTags, "" )}</div>`;
+
         return `
-            <div class="js-carousel-item instagram__item js-carousel-image -cover -column" data-img-src="${item.images.standard_resolution.url}"></div>
+            <div class="js-carousel-item instagram__item js-carousel-image -cover -column js-hover-item" data-img-src="${item.images.standard_resolution.url}">
+                <div class="grid__hover ghost -poino">
+                    <div class="grid__hover__el ghost__child">
+                        ${caption}
+                    </div>
+                </div>
+            </div>
         `;
 
     }).join( "" );
