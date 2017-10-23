fc900cac57910aaea3dd4bc507a3f01d33cdc6ff
kitajchuk <kitajchuk@gmail.com>
2017-10-23 16:02:15 -0700

    autoplay IG videos inline for the FEED

--- source/js/views/feed-layout.js
+++ source/js/views/feed-layout.js
@@ -9,11 +9,12 @@ export default ( items ) => {
         const padBottom = item.image.height / item.image.width * 100;
         const caption = item.caption ? `<div class="grid__caption p">${item.caption.replace( rTags, "" )}</div>` : null;
         const title = item.title ? `<div class="grid__title p p--h3">${item.title.replace( rTags, "" )}</div>` : null;
+        const media = (item.ig && item.video ? `<video class="-full" src="${item.video.url}" loop muted preload autoplay playsinline></video>` : `<div class="feed__aspect js-feed-image -cover" data-img-src="${item.image.url}" style="padding-bottom:${padBottom}%;"></div>`);
 
         return `
             <a href="${url}" class="grid__item feed__item -column animate-lift js-feed-anim js-hover-item ${jsClass}" data-id="${item.id}" ${target}>
                 <div class="animate__el -poino">
-                    <div class="feed__aspect js-feed-image -cover" data-img-src="${item.image.url}" style="padding-bottom:${padBottom}%;"></div>
+                    ${media}
                     <div class="feed__icon">${icon}</div>
                     <div class="grid__hover ghost">
                         <div class="grid__hover__el ghost__child">
