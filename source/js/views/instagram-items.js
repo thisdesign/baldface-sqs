db671edd4dc111e67887b8a7c672d105852660c3
kitajchuk <kitajchuk@gmail.com>
2017-11-01 09:52:59 -0700

    refactor grid tile hovers

--- source/js/views/instagram-items.js
+++ source/js/views/instagram-items.js
@@ -1,13 +1,13 @@
 export default ( json ) => {
     return json.data.map(( item ) => {
         const rTags = /(<([^>]+)>)/ig;
-        const caption = `<div class="grid__caption p">${item.caption.text.replace( rTags, "" )}</div>`;
+        const title = `<div class="grid__title p">${item.caption.text.replace( rTags, "" )}</div>`;
 
         return `
             <div class="js-carousel-item instagram__item js-carousel-image -cover -column js-hover-item" data-img-src="${item.images.standard_resolution.url}">
                 <div class="grid__hover ghost -poino">
                     <div class="grid__hover__el ghost__child">
-                        ${caption}
+                        ${title}
                     </div>
                 </div>
             </div>
