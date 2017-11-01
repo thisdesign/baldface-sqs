4a4915547a0b704c8952dc247d0ed42ffe60614f
kitajchuk <kitajchuk@gmail.com>
2017-11-01 09:52:45 -0700

    refactor social getInstagram

--- source/js/social.js
+++ source/js/social.js
@@ -1,5 +1,5 @@
 import $ from "properjs-hobo";
-import * as core from "./core";
+// import * as core from "./core";
 
 
 /**
@@ -15,35 +15,20 @@ const social = {
             userId: "297115335",
             accessToken: "297115335.3d264f9.68585bfbb56d4010afa93d099ea41978"
         };
-
-        this.loadInstagram();
     },
 
 
-    loadInstagram () {
-        $.ajax({
+    execTimeout () {},
+
+
+    getInstagram () {
+        return $.ajax({
             url: `https://api.instagram.com/v1/users/${this.instagram.userId}/media/recent`,
             method: "GET",
             dataType: "jsonp",
             data: {
                 access_token: this.instagram.accessToken
             }
-
-        }).then(( json ) => {
-            core.cache.set( "instagram", json );
-        });
-    },
-
-
-    getInstagram () {
-        return new Promise(( resolve ) => {
-            let json = core.cache.get( "instagram" );
-
-            while ( !json ) {
-                json = core.cache.get( "instagram" );
-            }
-
-            resolve( json );
         });
     }
 };
