928218b23f136edfc3d0855088cf14a97d2d4b2f
kitajchuk <kitajchuk@gmail.com>
2017-11-02 16:31:48 -0700

    more better webpackson

--- webpack.config.js
+++ webpack.config.js
@@ -10,20 +10,27 @@ const request = require( "request" );
 const lager = require( "properjs-lager" );
 const open = require( "open" );
 const local = "http://localhost:9000";
+const plugins = [
+    new webpack.LoaderOptionsPlugin({
+        options: {
+            postcss: [autoprefixer( { browsers: ["last 2 versions"] } )]
+        }
+    })
+];
 let isOpen = false;
 
 
 
-module.exports = {
-    devtool: "source-map",
-
-
-    plugins: [
-        new WebpackOnBuildPlugin(() => {
+module.exports = ( env ) => {
+    // Only handle builds for sandbox dev environment
+    if ( env.sandbox ) {
+        plugins.push(new WebpackOnBuildPlugin(() => {
+            // First build opens localhost for you
             if ( !isOpen ) {
                 isOpen = true;
                 open( local );
 
+            // Subsequent builds trigger SQS reloads
             } else {
                 request({
                     url: `${local}/local-api/reload/trigger`,
@@ -31,39 +38,41 @@ module.exports = {
 
                 }, () => lager.server( "local-api reload trigger..." ) );
             }
-        }),
-        new webpack.LoaderOptionsPlugin({
-            options: {
-                postcss: [autoprefixer( { browsers: ["last 2 versions"] } )]
-            }
-        })
-    ],
-
-
-    resolve: {
-        modules: [root, source, nodeModules],
-        mainFields: ["webpack", "browserify", "web", "hobo", "main"]
-    },
-
-
-    entry: {
-        "app": path.resolve( __dirname, "source/js/app.js" )
-    },
-
-
-    output: {
-        path: path.resolve( __dirname, "build", "scripts" ),
-        filename: "[name].js"
-    },
-
-
-    module: {
-        rules: [
-            { test: /source\/js\/.*\.js$/, exclude: /node_modules/, use: ["eslint-loader"], enforce: "pre" },
-            { test: /source\/js\/.*\.js$/, exclude: /node_modules/, use: [{ loader: "babel-loader", options: { presets: ["es2015"] } }] },
-            { test: /(hobo|hobo.build)\.js$/, use: ["expose-loader?hobo"] },
-            { test: /\.(sass|scss)$/, exclude: /node_modules/, use: ["file-loader?name=../styles/[name].css", "postcss-loader", "sass-loader"] },
-            { test: /svg-.*\.block$|\.svg$/, exclude: /node_modules/, use: ["svg-inline-loader"] }
-        ]
+        }));
     }
+
+    return {
+        devtool: "source-map",
+
+
+        plugins,
+
+
+        resolve: {
+            modules: [root, source, nodeModules],
+            mainFields: ["webpack", "browserify", "web", "hobo", "main"]
+        },
+
+
+        entry: {
+            "app": path.resolve( __dirname, "source/js/app.js" )
+        },
+
+
+        output: {
+            path: path.resolve( __dirname, "build", "scripts" ),
+            filename: "[name].js"
+        },
+
+
+        module: {
+            rules: [
+                { test: /source\/js\/.*\.js$/, exclude: /node_modules/, use: ["eslint-loader"], enforce: "pre" },
+                { test: /source\/js\/.*\.js$/, exclude: /node_modules/, use: [{ loader: "babel-loader", options: { presets: ["es2015"] } }] },
+                { test: /(hobo|hobo.build)\.js$/, use: ["expose-loader?hobo"] },
+                { test: /\.(sass|scss)$/, exclude: /node_modules/, use: ["file-loader?name=../styles/[name].css", "postcss-loader", "sass-loader"] },
+                { test: /svg-.*\.block$|\.svg$/, exclude: /node_modules/, use: ["svg-inline-loader"] }
+            ]
+        }
+    };
 };
