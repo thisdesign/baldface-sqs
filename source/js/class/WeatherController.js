120b8708e9137b81c2095513d1539e63bb2615df
kitajchuk <kitajchuk@gmail.com>
2017-11-16 13:21:34 -0800

    add weatherunderground data

--- source/js/class/WeatherController.js
+++ source/js/class/WeatherController.js
@@ -1,5 +1,5 @@
 // import * as core from "../core";
-// import $ from "properjs-hobo";
+import $ from "properjs-hobo";
 import weatherDataView from "../views/weather-data";
 
 
@@ -14,7 +14,23 @@ import weatherDataView from "../views/weather-data";
 class WeatherController {
     constructor ( element ) {
         this.element = element;
-        this.element[ 0 ].innerHTML = weatherDataView();
+        this.apiKey = "d183d92692749a09";
+        this.apiPWS = "ICENTRAL179";
+        this.apiUrl = `http://api.wunderground.com/api/${this.apiKey}/conditions/forecast/q/pws:${this.apiPWS}.json`;
+
+        this.load();
+    }
+
+
+    load () {
+        $.ajax({
+            url: this.apiUrl,
+            method: "GET",
+            dataType: "json"
+
+        }).then(( json ) => {
+            this.element[ 0 ].innerHTML = weatherDataView( json );
+        });
     }
 
 
--- source/js/views/weather-data.js
+++ source/js/views/weather-data.js
@@ -1,16 +1,23 @@
-export default ( /*data*/ ) => {
-    return `
+export default ( data ) => {
+    const forecast = data.forecast.simpleforecast.forecastday[ 0 ];
+    const conditions = data.current_observation;
+
+    return data.response.error ? `
+        <div class="weather__error -wrap">
+            <div class="hh1">${data.response.error}</div>
+        </div>
+    ` : `
         <div class="weather__primary -wrap">
             <div class="weather__temp -column -vtop">
                 <div class="weather__temp__curr">
-                    <div class="hh1 -column -vtop">-04</div>
+                    <div class="hh1 -column -vtop">${conditions.temp_c}</div>
                     <div class="h2 -column -vtop -deg">&deg;</div>
                     <div class="h2 -column -vtop -sup">C</div>
                 </div>
                 <div class="weather__stat p p--hh">
-                    <div>4&deg; High</div>
+                    <div>${forecast.high.celsius}&deg; High</div>
                     <span></span>
-                    <div>-7&deg; Low</div>
+                    <div>${forecast.low.celsius}&deg; Low</div>
                 </div>
             </div>
             <div class="weather__depth weather__paddle -column -vtop">
@@ -27,12 +34,12 @@ export default ( /*data*/ ) => {
         </div>
         <div class="weather__secondary -wrap">
             <div class="weather__wind-dir -column -vtop">
-                <div class="h2">SSW</div>
+                <div class="h2">${forecast.avewind.dir}</div>
                 <div class="p p--hh">Wind Direction</div>
             </div>
             <div class="weather__wind-speed weather__paddle -column -vtop">
                 <div class="weather__wind-speed__curr">
-                    <div class="h2 -column -vtop">11</div>
+                    <div class="h2 -column -vtop">${forecast.avewind.kph}</div>
                     <div class="h3 -column -vtop -sup">KPH</div>
                 </div>
                 <div class="p p--hh">Wind Speed</div>
