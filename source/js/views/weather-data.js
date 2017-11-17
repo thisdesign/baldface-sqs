ca28937f0b58e653ce202b046b3def13a6bae07c
kitajchuk <kitajchuk@gmail.com>
2017-11-17 09:10:00 -0800

    change out system units

--- source/js/views/weather-data.js
+++ source/js/views/weather-data.js
@@ -7,6 +7,8 @@ export default ( data, system ) => {
     const low = isMetric ? forecast.low.celsius : forecast.low.fahrenheit;
     const winddir = forecast.avewind.dir;
     const windspeed = isMetric ? forecast.avewind.kph : forecast.avewind.mph;
+    const tempunit = isMetric ? "C" : "F";
+    const speedunit = isMetric ? "KPH" : "MPH";
 
     return data.response.error ? `
         <div class="weather__error -wrap">
@@ -18,7 +20,7 @@ export default ( data, system ) => {
                 <div class="weather__temp__curr">
                     <div class="hh1 -column -vtop">${temp}</div>
                     <div class="h2 -column -vtop -deg">&deg;</div>
-                    <div class="h2 -column -vtop -sup">C</div>
+                    <div class="h2 -column -vtop -sup">${tempunit}</div>
                 </div>
                 <div class="weather__stat p p--hh">
                     <div>${high}&deg; High</div>
@@ -46,7 +48,7 @@ export default ( data, system ) => {
             <div class="weather__wind-speed weather__paddle -column -vtop">
                 <div class="weather__wind-speed__curr">
                     <div class="h2 -column -vtop">${windspeed}</div>
-                    <div class="h3 -column -vtop -sup">KPH</div>
+                    <div class="h3 -column -vtop -sup">${speedunit}</div>
                 </div>
                 <div class="p p--hh">Wind Speed</div>
             </div>
