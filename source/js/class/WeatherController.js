8ec6f1e96e46622d3b371ce6449f908ca4dcfdc4
kitajchuk <kitajchuk@gmail.com>
2017-11-16 17:57:49 -0800

    add metric vs imperial toggle

--- source/js/class/WeatherController.js
+++ source/js/class/WeatherController.js
@@ -14,11 +14,25 @@ import weatherDataView from "../views/weather-data";
 class WeatherController {
     constructor ( element ) {
         this.element = element;
+        this.system = "metric";
+        this.data = null;
         this.apiKey = "d183d92692749a09";
         this.apiPWS = "ICENTRAL179";
         this.apiUrl = `//api.wunderground.com/api/${this.apiKey}/conditions/forecast/q/pws:${this.apiPWS}.json`;
 
         this.load();
+        this.bind();
+    }
+
+
+    bind () {
+        this.element.on( "click", ".js-weather-toggle", ( e ) => {
+            const elem = $( e.target );
+            const data = elem.data();
+
+            this.system = data.system;
+            this.render();
+        });
     }
 
 
@@ -29,8 +43,14 @@ class WeatherController {
             dataType: "json"
 
         }).then(( json ) => {
-            this.element[ 0 ].innerHTML = weatherDataView( json );
+            this.data = json;
+            this.render();
         });
+    }
+
+
+    render () {
+        this.element[ 0 ].innerHTML = weatherDataView( this.data, this.system );
     }
 
 
--- source/js/views/weather-data.js
+++ source/js/views/weather-data.js
@@ -1,6 +1,12 @@
-export default ( data ) => {
+export default ( data, system ) => {
     const forecast = data.forecast.simpleforecast.forecastday[ 0 ];
     const conditions = data.current_observation;
+    const isMetric = (system === "metric");
+    const temp = isMetric ? conditions.temp_c : conditions.temp_f;
+    const high = isMetric ? forecast.high.celsius : forecast.high.fahrenheit;
+    const low = isMetric ? forecast.low.celsius : forecast.low.fahrenheit;
+    const winddir = forecast.avewind.dir;
+    const windspeed = isMetric ? forecast.avewind.kph : forecast.avewind.mph;
 
     return data.response.error ? `
         <div class="weather__error -wrap">
@@ -10,14 +16,14 @@ export default ( data ) => {
         <div class="weather__primary -wrap">
             <div class="weather__temp -column -vtop">
                 <div class="weather__temp__curr">
-                    <div class="hh1 -column -vtop">${conditions.temp_c}</div>
+                    <div class="hh1 -column -vtop">${temp}</div>
                     <div class="h2 -column -vtop -deg">&deg;</div>
                     <div class="h2 -column -vtop -sup">C</div>
                 </div>
                 <div class="weather__stat p p--hh">
-                    <div>${forecast.high.celsius}&deg; High</div>
+                    <div>${high}&deg; High</div>
                     <span></span>
-                    <div>${forecast.low.celsius}&deg; Low</div>
+                    <div>${low}&deg; Low</div>
                 </div>
             </div>
             <div class="weather__depth weather__paddle -column -vtop">
@@ -34,12 +40,12 @@ export default ( data ) => {
         </div>
         <div class="weather__secondary -wrap">
             <div class="weather__wind-dir -column -vtop">
-                <div class="h2">${forecast.avewind.dir}</div>
+                <div class="h2">${winddir}</div>
                 <div class="p p--hh">Wind Direction</div>
             </div>
             <div class="weather__wind-speed weather__paddle -column -vtop">
                 <div class="weather__wind-speed__curr">
-                    <div class="h2 -column -vtop">${forecast.avewind.kph}</div>
+                    <div class="h2 -column -vtop">${windspeed}</div>
                     <div class="h3 -column -vtop -sup">KPH</div>
                 </div>
                 <div class="p p--hh">Wind Speed</div>
@@ -47,6 +53,11 @@ export default ( data ) => {
         </div>
         <div class="weather__beaker">
             <div></div>
+        </div>
+        <div class="weather__toggle -wrap">
+            <div class="weather__toggle__system p -light -column ${isMetric ? "is-active" : ""} js-weather-toggle" data-system="metric">Metric</div>
+            <div class="p -light -column">&nbsp;/&nbsp;</div>
+            <div class="weather__toggle__system p -light -column ${isMetric ? "" : "is-active"} js-weather-toggle" data-system="imperial">Imperial</div>
         </div>
     `;
 };
