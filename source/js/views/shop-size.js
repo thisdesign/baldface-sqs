c6a96f107844c217a3c81b671608f925bf026cd5
kitajchuk <kitajchuk@gmail.com>
2017-11-29 18:17:45 -0800

    correctly filter size variants...

--- source/js/views/shop-size.js
+++ source/js/views/shop-size.js
@@ -1,10 +1,21 @@
 export default ( data ) => {
     const order = data.variantOptionOrdering[ 1 ];
+    const variants = [];
+
+    data.variants.forEach(( variant ) => {
+        const compare = variants.find(( vari ) => {
+            return vari.attributes[ order ] === variant.attributes[ order ];
+        });
+
+        if ( !compare ) {
+            variants.push( variant );
+        }
+    });
 
     return `
         <div class="p -meta -dark">${order}</div>
         <select class="product__selector js-shop-size-selector p -meta">
-            ${data.variants.map(( variant, i ) => {
+            ${variants.map(( variant, i ) => {
                 return `<option value="${variant.attributes[ order ]}" ${i === 0 ? "selected" : ""}>${variant.attributes[ order ]}</option>`;
 
             }).join( "" )}
