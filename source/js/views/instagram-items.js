8944b6426f982b8bfbaf11adc9d283e7b8d417fb
kitajchuk <kitajchuk@gmail.com>
2018-02-21 10:26:22 -0800

    adjust image layouts for instagram carousel belt

--- source/js/views/instagram-items.js
+++ source/js/views/instagram-items.js
@@ -4,7 +4,7 @@ export default ( json ) => {
         const title = `<div class="grid__title p">${item.caption.text.replace( rTags, "" )}</div>`;
 
         return `
-            <div class="js-carousel-item instagram__item js-carousel-image -cover -column js-hover-item" data-img-src="${item.images.standard_resolution.url}">
+            <div class="js-carousel-item instagram__item js-carousel-image -column js-hover-item" data-img-src="${item.images.standard_resolution.url}">
                 <div class="grid__hover ghost -poino">
                     <div class="grid__hover__el ghost__child">
                         ${title}
